1f584239427f31b32f46219bc46dc4a5db2f70e4
fangyuan <1109181887@qq.com>
2023-12-24 22:19:03 +0800

    11

--- server/src/common/manager/ProtoManager.ts
+++ server/src/common/manager/ProtoManager.ts
@@ -1,6 +1,13 @@
 import { SingleBase } from "../base/SingleBase";
 
 export class ProtoManager extends SingleBase{
+    constructor(){
+        super();
+        this.loadProto();
+    }
+    loadProto(){
+
+    }
     decode(cmd: number, msg: Buffer){
         return JSON.parse(msg.toString());
     }
