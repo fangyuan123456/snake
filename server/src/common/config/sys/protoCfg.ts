990fd597b2e6d8848eaf26294977e9454f4550fa
fangyuan <1109181887@qq.com>
2023-12-26 14:15:03 +0800

    111

--- server/src/common/config/sys/protoCfg.ts
+++ server/src/common/config/sys/protoCfg.ts
@@ -2,8 +2,8 @@ import { I_proto} from "mydog";
 
 export let ProtoCfg:{[key:string]:I_proto}={
     login:{
-        req:1000,
-        res:1001,
+        cs:1000,
+        sc:1001,
         route:""
     }
 }
--- server/src/common/manager/ProtoManager.ts
+++ server/src/common/manager/ProtoManager.ts
@@ -1,15 +1,16 @@
 import { SingleBase } from "../base/SingleBase";
+import * as proto from "../proto/proto";
 
 export class ProtoManager extends SingleBase{
     constructor(){
         super();
-        this.loadProto();
-    }
-    loadProto(){
-
     }
     decode(cmd: number, msg: Buffer){
-        return JSON.parse(msg.toString());
+        // let data = game.app.cmdConfig[cmd];
+        let funcName = game.app.getCmdMessageName(cmd);
+        // if(proto[funcName]){
+
+        // }
     }
     encode(cmd: number, msg: any){
         return Buffer.from(JSON.stringify(msg));
