ecf6a92fc8861ea460eb2eec546afa3c4e5abbfa
fangyuan <1109181887@qq.com>
2024-01-01 18:10:36 +0800

    111

--- proto/convertBuffer.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-call copy /b /y .\protoCmd\*.proto combined.proto
-call pbjs combined.proto --ts proto.ts
-call copy /y proto.ts ..\server\src\common\proto\proto.ts
-call copy /y proto.ts ..\assets\Script\common\proto\proto.ts
-
-pause
---
+++ tools/makeProto/convertBuffer.bat
@@ -0,0 +1,8 @@
+@echo off
+
+call copy /b /y .\protoCmd\*.proto combined.proto
+call pbjs combined.proto --ts proto.ts
+call copy /y proto.ts ..\..\server\src\common\proto\proto.ts
+call copy /y proto.ts ..\..\client\assets\Script\common\proto\proto.ts
+
+pause
