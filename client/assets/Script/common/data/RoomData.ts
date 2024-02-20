7359bf26a5b900416b330f4c15deaf3f8b9fd53d
fangyuan <1109181887@qq.com>
2024-02-20 22:20:24 +0800

    111

--- client/assets/Script/common/data/RoomData.ts
+++ client/assets/Script/common/data/RoomData.ts
@@ -1,6 +1,7 @@
 import { SocketType } from "../Game";
 import { CompBase } from "../base/CompBase";
 import DataBase from "../base/DataBase";
+import { SCENE_NAME } from "../base/SceneBase";
 import { Dic } from "../interface/I_Common";
 import { I_enterRoomRes, I_frameMsgRes } from "../interface/I_Game";
 import { I_roomInfo } from "../interface/I_Info";
@@ -9,9 +10,34 @@ export default class RoomData extends CompBase{
     frames:Dic<number[]>= {}
     start(): void {
         game.roomData = this;
+        this.enterRoom();
         game.netMgr.onMsg("frameMsg",(data)=>{
             this.onFrameMsg(data);
         },this,SocketType.game)
+    }
+    enterRoom(){
+        game.gameData.getRoomInfo((data)=>{
+            if(data.roomId<=0){
+                game.alertMgr.showTiShiBox({content:"对局已结束！",btnCallBackList:[
+                    {
+                        callBack:()=>{
+                            game.sceneMgr.changeScene(SCENE_NAME.MENUSCENE);
+                        }
+                    }
+                ]})
+                game.netMgr.closeAndDestroySocket(SocketType.game);
+            }else{
+                if(!game.netMgr.socketMap[SocketType.game]){
+                    game.netMgr.createSocket(data.roomIp,SocketType.game,true);
+                    game.netMgr.onReady(()=>{
+                        game.netMgr.sendSocket({msgHead:"enterRoom",msgData:{}},(data:I_enterRoomRes)=>{
+                            this.setRoomInfo(data);
+                            game.eventMgr.dispatch("gameInit");
+                        },this,SocketType.game)
+                    },this,SocketType.game)
+                }
+            }
+        },this);
     }
     setRoomInfo(roomInfo:I_enterRoomRes){
 
@@ -34,5 +60,6 @@ export default class RoomData extends CompBase{
     }
     protected onDestroy(): void {
         game.roomData = null;
+        game.netMgr.closeAndDestroySocket(SocketType.game);
     }
 }
--- client/assets/Script/scene/GameScene.ts
+++ client/assets/Script/scene/GameScene.ts
@@ -5,6 +5,7 @@
 // Learn life-cycle callbacks:
 import { SocketType } from "../common/Game";
 import { SCENE_NAME, SceneBase } from "../common/base/SceneBase";
+import RoomData from "../common/data/RoomData";
 import { I_enterRoomRes } from "../common/interface/I_Game";
 
 //  - https://docs.cocos.com/creator/2.4/manual/en/scripting/life-cycle-callbacks.html
@@ -16,38 +17,7 @@ export default class GameScene extends SceneBase {
         super(SCENE_NAME.GAMESCENE);
     }
     start () {
-        super.start();
-        this.init();   
-    }
-    init(){
-        game.gameData.getRoomInfo((data)=>{
-            if(data.roomId<=0){
-                game.alertMgr.showTiShiBox({content:"对局已结束！",btnCallBackList:[
-                    {
-                        callBack:()=>{
-                            game.sceneMgr.changeScene(SCENE_NAME.MENUSCENE);
-                        }
-                    }
-                ]})
-                game.netMgr.closeAndDestroySocket(SocketType.game);
-            }else{
-                if(!game.netMgr.socketMap[SocketType.game]){
-                    this.createGameSocket(data.roomIp);
-                    game.netMgr.onReady(()=>{
-                        game.netMgr.sendSocket({msgHead:"enterRoom",msgData:{}},(data:I_enterRoomRes)=>{
-                            game.roomData.setRoomInfo(data);
-                            game.eventMgr.dispatch("gameInit");
-                        },this,SocketType.game)
-                    },this,SocketType.game)
-                }
-            }
-        },this);
-    }
-    createGameSocket(ip){
-        game.netMgr.createSocket(ip,SocketType.game,true);
-    }
-    protected onDestroy(): void {
-        super.onDestroy();
-        game.netMgr.closeAndDestroySocket(SocketType.game);
+        super.start(); 
+        cc.find("Canvas").addComponent(RoomData)
     }
 }
